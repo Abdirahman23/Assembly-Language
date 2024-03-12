44e3bd4a1030670070de4443d583241b81293b96
Abdirahman Saed <77121312+Abdirahman23@users.noreply.github.com>
2024-03-12 20:45:47 +0500

    Calculator by using Assembly

--- assem.asm
+++ assem.asm
@@ -6,24 +6,62 @@ include irvine32.inc
 
 .data
 
-abudi BYTE "computer organization and assembly language is fun ", 0
+num1 DWORD ?
+num2 DWORD ?
+zum DWORD ?
+zub DWORD ?
+
+prompt BYTE "Enter two numbers: "
+prompt2 BYTE "Addition of above 2 numbers is: ", 0
+prompt3 BYTE "Subtraction of above 2 numbers is: ", 0
+
 
 .code
 main PROC
-MOV edx, 10
 
-looper:
-   MOV ecx , SIZEOF abudi
-   MOV esi,  OFFSET abudi
-   abduiii:
-      MOV eax, [esi]
-      Call writechar
-      add esi, 1 
-      loop abduiii
-   call crlf
-   sub edx, 1
-   MOV ecx, edx
-loop looper
+mov edx, OFFSET prompt
+call WriteString
+
+call ReadInt 
+mov num1, eax
+   
+call ReadInt 
+mov num2, eax
+
+;calculating addition
+
+mov eax,num1
+mov ebx,num2
+
+add eax,ebx
+
+mov zum,eax
+
+;calculating subraction
+
+mov eax,num1
+mov ebx,num2
+
+sub ebx,eax
+
+mov zub,eax
+
+;out putting the sum
+mov edx, OFFSET prompt2
+call WriteString
+
+mov eax, zum
+call WriteDec
+
+ call Crlf
+
+;out putting the sum
+mov edx, OFFSET prompt3
+call WriteString
+
+mov eax, zub
+call WriteDec
+
 
 
 call Dumpregs
